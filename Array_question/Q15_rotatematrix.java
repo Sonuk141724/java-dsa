18bc20a7ee24dbeae8a286f78b6cc33ca23a12a6
Sonuk141724 <161608484+Sonuk141724@users.noreply.github.com>
2024-09-19 14:43:26 +0000

    updated_q_15

--- Array_question/Q15_rotatematrix.java
+++ Array_question/Q15_rotatematrix.java
@@ -1,5 +1,16 @@
 package Array_question;
 
-public class Q15_rotatematrix {
+import java.util.Scanner;
+public class Q15_rotatematrix 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+        
+    }
     
 }
